5159c61b38daa16d1cefd96e4844d5161c16f405
syarzabal <90835403+syarzabal@users.noreply.github.com>
2025-06-12 16:44:20 +0200

    Funcion grabar

---
+++ backend/analizador_chunks.py
@@ -0,0 +1,17 @@
+import librosa
+from librosa import feature
+import numpy as np
+
+class AnalizadorChunks:
+    def __init__(self):
+        pass
+
+    def getTempo(self, audio_chunk: np.ndarray, sample_rate: int) -> float:
+        if audio_chunk is None or len(audio_chunk) == 0:
+            raise ValueError('Audio chunk vacío')
+
+        tempo = librosa.feature.tempo(y=audio_chunk, sr=sample_rate) # TODO: start_bpm, std_bpm, max_tempo
+        return float(tempo[0])
+
+    def testConexion(self) -> str:
+        return "Conexión establecida"
--- gui/pantalla_analisis_en_directo.py
+++ gui/pantalla_analisis_en_directo.py
@@ -1,14 +1,36 @@
 import tkinter as tk
 from tkinter import ttk
+from backend.analizador_chunks import AnalizadorChunks
+import sounddevice as sd
 
 class PantallaAnalisisDirecto(tk.Frame):
     def __init__(self, parent_widget, controller):
         super().__init__(parent_widget)
         self.controller = controller
 
-        label = ttk.Label(self, text="Calcular tempo en directo", font=("Segoe UI", 16), background="orange")
-        label.pack(pady=20)
+        self.duracion_chunk = 4 # Duracion en segundos de un chunk de audio
+        self.sample_rate = 44100
+        self.AnalizadorChunks = AnalizadorChunks()
 
-        btn_volver = ttk.Button(self, text="Volver al inicio",
+        self.label_test_conexion = ttk.Label(self, text="Conexion NO establecida")
+        self.label_test_conexion.config(text=self.AnalizadorChunks.testConexion())
+        self.label_test_conexion.pack(pady=20)
+
+        self.label = ttk.Label(self, text="Calcular tempo en directo", font=("Segoe UI", 16), background="orange")
+        self.label.pack(pady=20)
+
+        self.btn_volver = ttk.Button(self, text="Volver al inicio",
                                 command=lambda: controller.mostrar_pantalla("PantallaInicio"))
-        btn_volver.pack()
+        self.btn_volver.pack()
+
+
+    def grabar(self, duracion, sr):
+        audio = sd.rec(
+            int(duracion * sr),
+            samplerate=sr,
+            channels=1,
+            dtype='float32'
+        )
+        sd.wait()
+        audio = audio.flatten()
+        return audio
