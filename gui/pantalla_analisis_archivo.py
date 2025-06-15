81d563f7b9a8d811580308c59b45c437fc1a352b
syarzabal <90835403+syarzabal@users.noreply.github.com>
2025-06-15 13:39:36 +0200

    Primera implementacion de analisis de archivo

--- gui/pantalla_analisis_archivo.py
+++ gui/pantalla_analisis_archivo.py
@@ -1,23 +1,27 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+from backend.analizador_archivos import AnalizadorArchivos
 
 class PantallaAnalisisArchivo(tk.Frame):
 
     def __init__(self, parent_widget, controller):
         super().__init__(parent_widget)
         self.controller = controller
+        self.analizador = None
         self.archivo_seleccionado = None
+
         self._crear_widgets()
 
     def _crear_widgets(self):
         ttk.Button(self, text="⬅", command=lambda: self.controller.mostrar_pantalla("PantallaInicio")).place(x=5, y=5)
         ttk.Label(self, text="Analizar desde archivo", font=("Segoe UI", 16)).pack(pady=20)
+        ttk.Label(self, text="Para resultados más precisos se recomienda usar archivos .wav", foreground="gray").pack(pady=5)
 
         self.btn_elegir_archivo = ttk.Button(self, text="Seleccionar archivo", command=lambda: self._seleccionar_archivo())
         self.btn_elegir_archivo.pack(pady=5)
 
-        self.label_archivo_seleccionado = ttk.Label(self, text="Ningún archivo seleccionado")
+        self.label_archivo_seleccionado = ttk.Label(self, text="")
         self.label_archivo_seleccionado.pack(pady=5)
 
     def _seleccionar_archivo(self):
@@ -27,3 +31,4 @@ class PantallaAnalisisArchivo(tk.Frame):
         if ruta_archivo:
             self.archivo_seleccionado = ruta_archivo
             self.label_archivo_seleccionado.config(text=f"Archivo seleccionado: {ruta_archivo}")
+            self.analizador = AnalizadorArchivos(ruta_archivo)
