6a9236e8ad65a94a7f7cfc70d62d94f6139c5634
syarzabal <syarzabal@gmail.com>
2025-07-14 19:50:33 +0200

    Cambiar pady pantalla inicio

--- gui/pantalla_inicio.py
+++ gui/pantalla_inicio.py
@@ -12,8 +12,8 @@ class PantallaInicio(tk.Frame):
 
         btn_analisis_directo = ttk.Button(self, text="Ver tempo en directo",
                                   command=lambda: controller.mostrar_pantalla("PantallaAnalisisDirecto"))
-        btn_analisis_directo.pack(pady=30)
+        btn_analisis_directo.pack(pady=20)
 
         bton_analisis_archivo = ttk.Button(self, text="Analizar desde un archivo",
                                  command=lambda: controller.mostrar_pantalla("PantallaAnalisisArchivo"))
-        bton_analisis_archivo.pack(pady=30)
+        bton_analisis_archivo.pack(pady=20)
